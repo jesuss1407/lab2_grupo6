6fc611ccfcbfddaaaec992bbb1e03384e68dc92e
jesus <a20196324@pucp.edu.pe>
2022-09-13 20:51:30 -0500

    inin

---
+++ src/test/java/com/example/lab2_g6/Lab2G6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_g6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2G6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
