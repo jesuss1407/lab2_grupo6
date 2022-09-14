4e4413bf9bafcc32a5ec6ff17fd2f912040f685c
ArturoManuel <noriegaarturonoriega@gmail.com>
2022-09-14 07:20:22 -0500

    cambios menores

--- src/main/java/com/example/lab2_g6/controller/TransactionController.java
+++ src/main/java/com/example/lab2_g6/controller/TransactionController.java
@@ -1,15 +1,18 @@
 package com.example.lab2_g6.controller;
 
+import com.example.lab2_g6.entity.Transaction;
 import com.example.lab2_g6.repository.CurrencyRepository;
 import com.example.lab2_g6.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("transaption")
+@RequestMapping("transaccion")
 public class TransactionController {
     @Autowired
     TransactionRepository transactionRepository;
@@ -30,5 +33,13 @@ public class TransactionController {
         model.addAttribute("listaTransapcioes",transactionRepository.buscarTrapsactiones());
         return "form";
     }
+
+
+    @PostMapping("/save")
+    public String guardarNuevoTransportista(Transaction transaction, RedirectAttributes attr) {
+        transactionRepository.save(transaction);
+        return "redirect:/transaccion/list";
+    }
+
 
 }
