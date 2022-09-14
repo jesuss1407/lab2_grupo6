efe1a322d714d78a5fd7d8ecebe99aa3ef3221f8
ArturoManuel <noriegaarturonoriega@gmail.com>
2022-09-14 07:13:54 -0500

    pregunta 4 - listo

--- src/main/java/com/example/lab2_g6/controller/TransactionController.java
+++ src/main/java/com/example/lab2_g6/controller/TransactionController.java
@@ -1,5 +1,6 @@
 package com.example.lab2_g6.controller;
 
+import com.example.lab2_g6.repository.CurrencyRepository;
 import com.example.lab2_g6.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,11 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TransactionController {
     @Autowired
     TransactionRepository transactionRepository;
-
+    @Autowired
+    CurrencyRepository currencyRepository;
     @GetMapping(value = {"/list"})
     public String listarTransportistas(Model model){
         model.addAttribute("listaTransaption",transactionRepository.findAll());
-        return "wallet";
+        return "transacciones";
+    }
+
+    @GetMapping("/new")
+    public String nuevoProductoFrm(Model model) {
+
+        model.addAttribute("listaCriptomonedas", transactionRepository.buscarCriptomonedas());
+        model.addAttribute("listaAmount",transactionRepository.buscarCantidad() );
+        model.addAttribute("listaNetwork",transactionRepository.buscarNetwork());
+        model.addAttribute("listaTransapcioes",transactionRepository.buscarTrapsactiones());
+        return "form";
     }
 
 }
